7e68432dd4d4e2d46771a00bbe04baa7c30e622d
almightobasadre <alau.basadre.up@phinmaed.com>
2024-09-13 21:23:46 +0800

    Changed Color Palette

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import Header from '../components/layout/header'
+import React from 'react';
+import Header from '../components/layout/header';
+import ColorDisplay from '../components/ui/colorDisplay';
 
 export const Home = () => {
   return (
     <div>
-        <Header>{/* Displays the Header component here */}</Header>
-        <div className='flex flex-col items-center justify-center min-h-[calc(100vh-80px)]'>
-            <h1 className='text-4xl font-bold'>Hello World!</h1>
-        </div>
+      <Header />
+      <div className="flex flex-col items-center justify-center min-h-[calc(100vh-80px)] p-4">
+        <h1 className="text-4xl font-bold mb-8">Hello World!</h1>
+        <ColorDisplay />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/app/styles/theme.ts
+++ src/app/styles/theme.ts
@@ -1,4 +1,3 @@
-
 // Define a theme with colors, spacing, etc.
 const theme = {
     colors: {
